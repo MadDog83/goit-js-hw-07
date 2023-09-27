353b368de4ccdf75d067f33671634efe8902a9d7
MadDog83 <130376768+MadDog83@users.noreply.github.com>
2023-09-27 22:03:15 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,70 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-// Отримуємо список ul.gallery
-const list = document.querySelector(".gallery");
-// Використовуємо map() для створення масиву елементів li на основі масиву galleryItems
-const items = galleryItems.map(item => {
-  // Створюємо елемент li
-  const listItem = document.createElement("li");
-  // Додаємо йому клас gallery__item
-  listItem.classList.add("gallery__item");
-  // Створюємо елемент a
-  const link = document.createElement("a");
-  // Додаємо йому клас gallery__link і атрибут href з посиланням на оригінальне зображення
-  link.classList.add("gallery__link");
-  link.href = item.original;
-  // Створюємо елемент img
-  const image = document.createElement("img");
-  // Додаємо йому клас gallery__image і атрибути src, data-source і alt з даними з масиву galleryItems
-  image.classList.add("gallery__image");
-  image.src = item.preview;
-  image.dataset.source = item.original;
-  image.alt = item.description;
-  // Додаємо елемент img до елемента a
-  link.append(image);
-  // Додаємо елемент a до елемента li
-  listItem.append(link);
-  // Повертаємо елемент li
-  return listItem;
-});
-// Вставляємо усі елементи li за одну операцію у список ul.gallery
-list.append(...items);
 
-// Додаємо обробник події click на список ul.gallery
-list.addEventListener("click", event => {
-    // Запобігаємо переходу по посиланню за замовчуванням
-    event.preventDefault();
-    // Отримуємо елемент, на який було клікнуто
-    const target = event.target;
-    // Якщо цей елемент має клас gallery__image
-    if (target.classList.contains("gallery__image")) {
-      // Отримуємо значення атрибута data-source цього елемента
-      const url = target.dataset.source;
-      // Викликаємо функцію openModal і передаємо їй url
-      openModal(url);
+// Отримання посилання на список галереї
+const galleryList = document.querySelector('.gallery');
+
+// Створення розмітки галереї
+const galleryMarkup = galleryItems.map(item => {
+  return `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`;
+}).join('');
+
+// Додавання розмітки до списку галереї
+galleryList.innerHTML = galleryMarkup;
+
+// Додавання слухача подій для відкриття модального вікна
+galleryList.addEventListener('click', event => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const largeImageURL = event.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${largeImageURL}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  // Закриття модального вікна після натискання клавіші Escape
+  window.addEventListener('keydown', event => {
+    if (event.key === 'Escape') {
+      instance.close();
+      window.removeEventListener('keydown', event);
     }
   });
-
- // Створюємо змінну для збереження посилання на екземпляр модального вікна
-let instance;
-
-// Створюємо функцію openModal, яка приймає один параметр - url великого зображення
-function openModal(url) {
-  // Використовуємо метод basicLightbox.create() для створення екземпляру модального вікна з розміткою, яка містить елемент img з атрибутом src рівним url
-  instance = basicLightbox.create(`
-    <img src="${url}" width="800" height="600">
-  `);
-  // Використовуємо метод show() для відкриття модального вікна
-  instance.show();
-}
-
-// Додаємо обробник події keydown на window
-window.addEventListener("keydown", event => {
-  // Якщо код натиснутої клавіші дорівнює "Escape"
-  if (event.code === "Escape") {
-    // Використовуємо метод close() для закриття модального вікна
-    instance.close();
-  }
 });
+
 console.log(basicLightbox);
 console.log(galleryItems);
