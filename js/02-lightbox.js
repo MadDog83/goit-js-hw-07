a59d88780d1c1735545bc0ea1ea9e9f74b2f5ac9
MadDog83 <130376768+MadDog83@users.noreply.github.com>
2023-09-28 19:10:28 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// Отримання посилання на список галереї
+const galleryList = document.querySelector('.gallery');
+
+// Створення розмітки галереї
+const galleryMarkup = galleryItems.map(item => {
+  return `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>
+  </li>`;
+}).join('');
+
+// Додавання розмітки до списку галереї
+galleryList.innerHTML = galleryMarkup;
+
+// Ініціалізація бібліотеки SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
 console.log(galleryItems);
